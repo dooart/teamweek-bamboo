8cd5d41ed21138ba0b198e3fa4f7294e0c0958cc
thiago <371426+dooart@users.noreply.github.com>
2019-05-25 12:34:02 +0100

    Refactor main script file
    There was too much code in the main js file. The checker deserves a new module. The main script will now only parse the arguments and invoke the checker.

---
+++ checker.js
@@ -0,0 +1,114 @@
+const _ = require("lodash");
+const moment = require("moment");
+const getTeamweekData = require("./teamweek.js");
+const getBambooData = require("./bamboo.js");
+
+const DATE_FORMAT = "YYYY-MM-DD";
+const normalizeVacations = (vacations, period) => {
+  return vacations
+    .map(vacation => {
+      let start = moment(vacation.start || vacation.start_date, DATE_FORMAT);
+      let end = moment(vacation.end || vacation.end_date, DATE_FORMAT);
+      const periodStart = moment(period.startDate, DATE_FORMAT);
+      const periodEnd = moment(period.endDate, DATE_FORMAT);
+      if (start.isBefore(periodStart, "day")) {
+        start = periodStart;
+      }
+      if (end.isAfter(periodEnd, "day")) {
+        end = periodEnd;
+      }
+      return { start: start.format(DATE_FORMAT), end: end.format(DATE_FORMAT) };
+    })
+    .map(vacation => `${vacation.start} to ${vacation.end}`);
+};
+
+const compare = (teamweek, bamboo, period) => {
+  const errors = [];
+  const teamweekPeople = Object.keys(teamweek);
+  const bambooPeople = Object.keys(bamboo);
+
+  const missingAllInTeamweek = _.difference(bambooPeople, teamweekPeople);
+  if (missingAllInTeamweek.length) {
+    errors.push(
+      `Teamweek missing all vacations for: \n - ${missingAllInTeamweek.join(
+        "\n - "
+      )}`
+    );
+  }
+
+  const missingAllInBamboo = _.difference(teamweekPeople, bambooPeople);
+  if (missingAllInBamboo.length) {
+    errors.push(
+      `Bamboo missing all vacations for: \n - ${missingAllInBamboo.join(
+        "\n - "
+      )}`
+    );
+  }
+
+  const people = _.intersection(teamweekPeople, bambooPeople);
+  people.sort().forEach(person => {
+    const vacationsTeamweek = teamweek[person];
+    const vacationsBamboo = bamboo[person];
+
+    const missingInTeamweek = _.difference(
+      normalizeVacations(vacationsBamboo, period),
+      normalizeVacations(vacationsTeamweek, period)
+    ).sort();
+    if (missingInTeamweek.length) {
+      errors.push(
+        `Teamweek missing ${person}'s vacations: \n - ${missingInTeamweek.join(
+          "\n - "
+        )}`
+      );
+    }
+
+    const missingInBamboo = _.difference(
+      normalizeVacations(vacationsTeamweek, period),
+      normalizeVacations(vacationsBamboo, period)
+    ).sort();
+    if (missingInBamboo.length) {
+      errors.push(
+        `Bamboo missing ${person}'s vacations: \n - ${missingInBamboo.join(
+          "\n - "
+        )}`
+      );
+    }
+  });
+
+  return errors;
+};
+
+const checker = async config => {
+  const period = {
+    startDate:
+      config.startDate ||
+      moment()
+        .startOf("year")
+        .format(DATE_FORMAT),
+    endDate:
+      config.endDate ||
+      moment()
+        .endOf("year")
+        .format(DATE_FORMAT)
+  };
+
+  const teamweekParams = {
+    ...config.teamweek,
+    startDate: period.startDate + "T00:00:00.000Z",
+    endDate: period.endDate + "T00:00:00.000Z"
+  };
+
+  const bambooParams = {
+    ...config.bamboo,
+    auth: `Basic ${Buffer.from(config.bamboo.auth + ":x").toString("base64")}`,
+    ...period
+  };
+
+  const teamweekData = await getTeamweekData(teamweekParams);
+  const bambooData = await getBambooData(bambooParams);
+
+  const errors = compare(teamweekData, bambooData, period);
+  errors.forEach(error => console.log(error + "\n"));
+};
+
+module.exports = checker;
--- index.js
+++ index.js
@@ -1,118 +1,5 @@
-const _ = require("lodash");
 const fs = require("fs");
-const moment = require("moment");
-const getTeamweekData = require("./teamweek.js");
-const getBambooData = require("./bamboo.js");
-
-const DATE_FORMAT = "YYYY-MM-DD";
-
-const normalizeVacations = (vacations, period) => {
-  return vacations
-    .map(vacation => {
-      let start = moment(vacation.start || vacation.start_date, DATE_FORMAT);
-      let end = moment(vacation.end || vacation.end_date, DATE_FORMAT);
-      const periodStart = moment(period.startDate, DATE_FORMAT);
-      const periodEnd = moment(period.endDate, DATE_FORMAT);
-      if (start.isBefore(periodStart, "day")) {
-        start = periodStart;
-      }
-      if (end.isAfter(periodEnd, "day")) {
-        end = periodEnd;
-      }
-      return { start: start.format(DATE_FORMAT), end: end.format(DATE_FORMAT) };
-    })
-    .map(vacation => `${vacation.start} to ${vacation.end}`);
-};
-
-const compare = (teamweek, bamboo, period) => {
-  const errors = [];
-  const teamweekPeople = Object.keys(teamweek);
-  const bambooPeople = Object.keys(bamboo);
-
-  const missingAllInTeamweek = _.difference(bambooPeople, teamweekPeople);
-  if (missingAllInTeamweek.length) {
-    errors.push(
-      `Teamweek missing all vacations for: \n - ${missingAllInTeamweek.join(
-        "\n - "
-      )}`
-    );
-  }
-
-  const missingAllInBamboo = _.difference(teamweekPeople, bambooPeople);
-  if (missingAllInBamboo.length) {
-    errors.push(
-      `Bamboo missing all vacations for: \n - ${missingAllInBamboo.join(
-        "\n - "
-      )}`
-    );
-  }
-
-  const people = _.intersection(teamweekPeople, bambooPeople);
-  people.sort().forEach(person => {
-    const vacationsTeamweek = teamweek[person];
-    const vacationsBamboo = bamboo[person];
-
-    const missingInTeamweek = _.difference(
-      normalizeVacations(vacationsBamboo, period),
-      normalizeVacations(vacationsTeamweek, period)
-    ).sort();
-    if (missingInTeamweek.length) {
-      errors.push(
-        `Teamweek missing ${person}'s vacations: \n - ${missingInTeamweek.join(
-          "\n - "
-        )}`
-      );
-    }
-
-    const missingInBamboo = _.difference(
-      normalizeVacations(vacationsTeamweek, period),
-      normalizeVacations(vacationsBamboo, period)
-    ).sort();
-    if (missingInBamboo.length) {
-      errors.push(
-        `Bamboo missing ${person}'s vacations: \n - ${missingInBamboo.join(
-          "\n - "
-        )}`
-      );
-    }
-  });
-
-  return errors;
-};
-
-const run = async config => {
-  const period = {
-    startDate:
-      config.startDate ||
-      moment()
-        .startOf("year")
-        .format(DATE_FORMAT),
-    endDate:
-      config.endDate ||
-      moment()
-        .endOf("year")
-        .format(DATE_FORMAT)
-  };
-
-  const teamweekParams = {
-    ...config.teamweek,
-    auth: `Bearer ${config.teamweek.auth}`,
-    startDate: period.startDate + "T00:00:00.000Z",
-    endDate: period.endDate + "T00:00:00.000Z"
-  };
-
-  const bambooParams = {
-    ...config.bamboo,
-    auth: `Basic ${Buffer.from(config.bamboo.auth + ":x").toString("base64")}`,
-    ...period
-  };
-
-  const teamweekData = await getTeamweekData(teamweekParams);
-  const bambooData = await getBambooData(bambooParams);
-
-  const errors = compare(teamweekData, bambooData, period);
-  errors.forEach(error => console.log(error + "\n"));
-};
+const checker = require("./checker.js");
 
 if (!fs.existsSync("./config.json")) {
   console.error("config.json file not found");
@@ -129,8 +16,6 @@ const argv = require("yargs")
   .option("endDate", {
     describe: "Search vacations up to this date. Format: YYYY-MM-DD"
   })
-  .option("teamweek.auth", {
-    describe: "Teamweek authentication token."
   .option("teamweek.authToken", {
     describe:
       "Teamweek authentication token. If you don't have a token, use your login credentials with authUser and authPassword."
@@ -151,7 +36,6 @@ const argv = require("yargs")
   })
   .option("teamweek.groupId", {
     describe:
-      "Optional. The id of a user group in Teamweek, in case you don't want to compare the whole workspace."
       "The id of the user group in Teamweek containing the people whose vacations you want to check."
   })
   .option("bamboo.auth", {
@@ -162,4 +46,4 @@ const argv = require("yargs")
   })
   .help().argv;
 
-run(argv);
+checker(argv);
